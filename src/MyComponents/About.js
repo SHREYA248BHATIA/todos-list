04f1b56defea48470ded44bdc035dc8c90dd2553
SHREYA248BHATIA <shreya2408bhatia@gmail.com>
2021-08-04 17:05:47 +0530

    changes done

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
 export const About = () => {
+    let newStyle={
+        margin:"250px auto 310px"
+      }
     return (
-        <div>
+        
+        <div style={newStyle}>
            <center> <h2>You are currently at about page </h2></center>
             
             <p>
             <center>   For making your own list click at <b>Home</b> above</center>
             </p>
         </div>
-    )
+      
+       
+    );
+    
 }
