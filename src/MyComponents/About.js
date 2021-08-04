8b6a2f28b6939a11d417269aaa1eaf18225f947b
SHREYA248BHATIA <shreya2408bhatia@gmail.com>
2021-08-04 14:12:46 +0530

    done

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -3,12 +3,11 @@ import React from 'react'
 export const About = () => {
     return (
         <div>
-            This is an about component
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                Tempore eaque quaerat, omnis, unde soluta consequatur non officiis iure, nisi 
-                molestiae illo exercitationem voluptas rem vel temporibus cum nulla alias voluptates
-                sed incidunt cupiditate voluptatum.
-                </p>
+           <center> <h2>You are currently at about page </h2></center>
+            
+            <p>
+            <center>   For making your own list click at <b>Home</b> above</center>
+            </p>
         </div>
     )
 }
